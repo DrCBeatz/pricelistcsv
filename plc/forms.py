90fbe2056dfd896124f97a1cde537b4873d35d96
drcbeatz <brynecarruthers@gmail.com>
2022-09-10 12:21:36 +0000

    created AJAX callback functions for output_csv_form and run on page load

--- plc/forms.py
+++ plc/forms.py
@@ -11,7 +11,9 @@ class CreatePriceListForm(forms.ModelForm):
         model = PriceList
         fields = ['title', 'file']
 
+
 class CreateOutputCsvForm(forms.ModelForm):
     class Meta:
         model = OutputCsv
         fields = ['title', 'product_list', 'price_list', 'product_list_search_field', 'product_list_replace_field', 'price_list_search_field', 'price_list_replace_field']
+        widgets = { 'product_list_search_field': forms.Select(), 'product_list_replace_field': forms.Select(), 'price_list_search_field': forms.Select(), 'price_list_replace_field': forms.Select(),}
