a71978e41baee421d3f748c5ced6fb0511824c77
drcbeatz <brynecarruthers@gmail.com>
2022-09-12 00:12:42 +0000

    added DiscountCode and PriceDiscount models

--- plc/admin.py
+++ plc/admin.py
@@ -1,6 +1,8 @@
-from plc.models import ProductList, PriceList, OutputCsv
+from plc.models import ProductList, PriceList, OutputCsv, DiscountCode, PriceDiscount
 from django.contrib import admin
 
 admin.site.register(ProductList)
 admin.site.register(PriceList)
 admin.site.register(OutputCsv)
+admin.site.register(DiscountCode)
+admin.site.register(PriceDiscount)
--- plc/models.py
+++ plc/models.py
@@ -45,3 +45,34 @@ class OutputCsv(models.Model):
     )
     def __str__(self):
         return self.title
+
+class DiscountCode(models.Model):
+    title = models.CharField(max_length=200,
+            validators=[MinLengthValidator(1, "Title must be greater than 1 characters")]
+    )
+    amount = models.CharField(max_length=20,
+            validators=[MinLengthValidator(2, "Title must be greater than 2 characters")], blank=True, null=True
+    )
+    cost = ArrayField(
+       models.FloatField(), blank=True, null=True
+    )
+    tier3_only = models.BooleanField(default=False)
+    def __str__(self):
+        return self.title
+
+class PriceDiscount(models.Model):
+    title = models.CharField(max_length=200,
+            validators=[MinLengthValidator(1, "Title must be greater than 1 characters")]
+    )
+
+    tier3_amount = ArrayField(
+       models.FloatField(), blank=True, null=True
+    )
+    tier2_amount = ArrayField(
+       models.FloatField(), blank=True, null=True
+    )
+    tier1_amount = ArrayField(
+       models.FloatField(), blank=True, null=True
+    )
+    def __str__(self):
+        return self.title
