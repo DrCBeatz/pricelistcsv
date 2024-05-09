83ac2a975362bb075b1a6dcb9baee71b0c0ea8e7
Bryne Carruthers <63172116+DrCBeatz@users.noreply.github.com>
2024-05-08 23:50:19 -0400

    Update image_download.py

--- image_download.py
+++ image_download.py
@@ -1,27 +1,36 @@
 import requests
-import shutil
 import pandas as pd
-import os
+import time
 
-input_file = 'products6.csv'
-output_file = 'products_imgs.zip'
+MODEL_KEY = 'Variant SKU'
+IMAGE_KEY = 'Product Image'
+INPUT_FILE = 'accessories_output.csv'
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
 
-df = pd.read_csv(input_file)
+def download_images_from_csv(input_file, model_key, image_key, headers):
+    df = pd.read_csv(input_file)
 
-for i in range(len(df.index)):
-    model = df.iloc[i]['model']
-    img_url = df.iloc[i]['image']
-    file_name = f'{model}.jpg'
-    print(model, img_url)
-    res = requests.get(img_url, stream=True)
+    for _, row in df.iterrows():
+        model = row[model_key]
+        img_url = row[image_key]
+        file_name = f'{model}.jpg'
+        print(model, img_url)
+        
+        try:
+            res = requests.get(img_url, headers=headers, stream=True)
+            if res.status_code == 200:
+                with open(file_name, 'wb') as f:
+                    for chunk in res.iter_content(chunk_size=8192):
+                        f.write(chunk)
+                print('Image downloaded:', file_name)
+            else:
+                print('Image could not be downloaded:', res.status_code)
+            res.close()
+            time.sleep(1)  # sleep for 1 second
+        except requests.RequestException as e:
+            print(f"Error downloading {img_url}. Error: {e}")
 
-    if res.status_code == 200:
-        with open(file_name, 'wb') as f:
-            shutil.copyfileobj(res.raw, f)
-            print('Image downloaded: ', file_name)
-    else:
-        print('Image could not be downloaded')
-
-os.system(f'rm {output_file}')
-os.system(f'zip {output_file} *.jpg')
-os.system("rm *.jpg")
+if __name__ == "__main__":
+    download_images_from_csv(INPUT_FILE, MODEL_KEY, IMAGE_KEY, HEADERS)
